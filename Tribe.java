25b653ee3848203ed7fa93c2917c599187d75195
Richie and Robert <glennonr@moravian.edu>
2021-02-23 10:50:10 -0500

    Added healer to enum and tribe

--- Tribe.java
+++ Tribe.java
@@ -19,11 +19,13 @@ public class Tribe
         nationName = nation;
         tribeName = tribe;
         tribeLifePoints = lifePoints;
-        for(int i = 0; i < 2; i++)
-            if(i % 2 == 0)
+        for(int i = 0; i < 3; i++)
+            if(i % 3 == 0)
                 members.add(new People(nationName, tribeName, PeopleType.warrior, tribeLifePoints / 2));
-            else
+            else if(i % 3 == 1)
                 members.add(new People(nationName, tribeName, PeopleType.wizzard, tribeLifePoints / 2));
+            else
+                members.add(new People(nationName, tribeName, PeopleType.healer, tribeLifePoints / 2));
         for(int i = 0; i < members.size(); i++)
             livingMembers.addAll(members);
     }
